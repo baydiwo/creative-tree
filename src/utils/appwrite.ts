bec5db50dc9be08706cb7625d44969429e50417e
Bayu Adi Wibowo <bayu.eighted@gmail.com>
2026-06-21 00:10:23 +0700

    fix

--- src/utils/appwrite.ts
+++ src/utils/appwrite.ts
@@ -1,7 +1,7 @@
 import { Client, Account, ID, type Models } from 'appwrite';
 
-const endpoint = 'https://<REGION>.cloud.appwrite.io/v1';
-const projectId = '<PROJECT_ID>';
+const endpoint = 'https://sgp.cloud.appwrite.io/v1';
+const projectId = '6a36c07000389c34024d';
 
 if (!endpoint || !projectId || endpoint.includes('<REGION>') || projectId.includes('<PROJECT_ID>')) {
   console.warn('Missing Appwrite endpoint and project ID. Please update src/utils/appwrite.ts');
